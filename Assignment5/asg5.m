a7b04e347ab468016310b00424887a80957c7ca6
VivekUdupa <vkoodli@clemson.edu>
2018-10-25 03:50:13 -0400

    Draft1

--- Assignment5/asg5.m
+++ Assignment5/asg5.m
@@ -16,22 +16,22 @@ n = length(mesData);
 %Plot the raw dataset
 x = 1:n;
 
-figure (1)
-plot(x,mesData, "O")
-hold on
-plot(x, trueData)
-hold off
-xlabel("time samples");
-ylabel("amplitude");
-set(gca, "FontSize",26);
-legend("Measurement data", "Actual position");
+% figure (1)
+% plot(x,mesData, "O")
+% hold on
+% plot(x, trueData)
+% hold off
+% xlabel("time samples");
+% ylabel("amplitude");
+% set(gca, "FontSize",26);
+% legend("Measurement data", "Actual position");
 
 %Initialize variables
 
 t = 1; %time
 
 %Noises
-mesNoise = 0.01;
+mesNoise = 0.0001;
 dynNoise = 0.001;
 
 % Initialize matrices 
@@ -53,13 +53,13 @@ R = mesNoise;
 I = eye(3) ;
 
 %State Co-variance
-S_t1_t1 = I;  
+SPrev = I;  
 
 %State Matrix
-X_t1_t1 = [0.001; 0.01; mesData(1,1)];
+XPrev = [0.001; 0.01; mesData(1,1)];
 
 %Result array
-result = zeros(1, n);
+Output = zeros(1, n);
 
 %DeltaT
 delta = 0.001;
@@ -67,57 +67,55 @@ delta = 0.001;
 while(t <  n)
     
     %Predicting the next state
-    X_t_t1 = [ (X_t1_t1(1,1) + (delta*(t-1) * X_t1_t1(2,1))) ;
-                X_t1_t1(2,1);
-                sin( X_t1_t1(1,1) * 0.1) ];
+    XPredict = [ (XPrev(1,1) + (delta*(t-1) * XPrev(2,1))) ;
+                XPrev(2,1);
+                sin( XPrev(1,1) * 0.1) ];
  
-    Dfx = [1 delta*t 0; 0 1 0; 0.1*cos(0.1 * X_t_t1(1,1)) 0 0];
+    Dfx = [1 delta*t 0; 0 1 0; 0.1*cos(0.1 * XPredict(1,1)) 0 0];
             
     %Predicting next state co-variance
-    S_t_t1 = (Dfx * S_t1_t1 * Dfx') + (Dfa * Q * Dfa');
+    SPredict = (Dfx * SPrev * Dfx') + (Dfa * Q * Dfa');
     
     %Measurement data
     Y_t = mesData(t);
     
     %Calculating kalman gain
-    K_t = (S_t_t1 * Dgx') / ( (Dgx * S_t_t1 * Dgx') + (Dgn * R * Dgn') );
+    K_t = (SPredict * Dgx') / ( (Dgx * SPredict * Dgx') + (Dgn * R * Dgn') );
     
     %Update state
-    X_t_t = X_t_t1 + (K_t * (Y_t - X_t_t1(3,1) ));
+    XNext = XPredict + (K_t * (Y_t - XPredict(3,1) ));
     
     %Update state co-variance
-    S_t_t = (I - K_t * (Dgx)) * S_t_t1;
+    S_t_t = (I - K_t * (Dgx)) * SPredict;
     
     %Update prev variables
-    S_t1_t1 = S_t_t;
-    X_t1_t1 = X_t_t;
+    SPrev = S_t_t;
+    XPrev = XNext;
     
-    result(1,t) = X_t_t(3,1);
+    Output(1,t) = XNext(3,1);
         
     %Incriment loop counter
     t = t  + 1;
 
 end
 x = 1:n;
-close all
+% close all
 
 figure (2)
-plot(x,mesData, "O")
+plot(x,mesData, "kx", "LineWidth", 2)
 hold on
-plot(x, result(1,:)')
+plot(x, Output(1,:), "K", "Linewidth",2)
 % hold on
-plot(x, trueData)
+plot(x, trueData, "k-.","Linewidth",2)
 hold off
-xlabel("time samples");
-ylabel("amplitude");
+xlabel("Samples");
+ylabel("Amplitude");
 set(gca, "FontSize",26);
 legend("Measurement data", "Filter Output", "True Position");
-
-
-
-
+xticks([0:130:780])
+axis([0 780 -2 2]);
 
 % %Ratios
-% dynNoise = 0.1 mesNoise = 0.01
-% dynNoise = 0.01 mesNoise = 0.001
+% dynNoise = 0.001 mesNoise = 1
 % dynNoise = 0.001 mesNoise = 0.1
+% dynNoise = 0.001 mesNoise = 0.003
