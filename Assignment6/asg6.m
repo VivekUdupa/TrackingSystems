ef6c6d68029655ec176f30ed6088b22e09c5dc22
VivekUdupa <vkoodli@clemson.edu>
2018-11-16 02:18:31 -0500

    Version 1

--- Assignment6/asg6.m
+++ Assignment6/asg6.m
@@ -7,21 +7,9 @@ actualPos = T(:,1);
 actualVel = T(:,2);
 sensorRead = T(:,3);
 
-X = 0 : length(actualPos) - 1;
- 
-% figure(1)
-% plot(X,actualPos);
-% xlabel("Samples");
-% ylabel("Actual Position");
-% 
-% 
-% figure(2)
-% plot (X, sensorRead);
-% xlabel("Samples");
-% ylabel("Sensor Reading");
 
 %Number of Particles
-M = 500;
+M = 10;
 
 %Initialization
 
@@ -44,9 +32,16 @@ wts = ones(1,M) * 1/M;
 wtsPrev = ones(1,M) * 1/M;
 wtsUN = ones(1,M) * 1/M;
 
-ytP = 0;
+ytP = zeros(1,M);
 
-for t = 1:length(X)
+%Resampling
+Q = zeros(1,M);
+T = zeros(1,M+1);
+
+%Output
+output = zeros(1,length(sensorRead));
+
+for t = 1:length(sensorRead)
     
     for i = 1:M
             
@@ -57,40 +52,117 @@ for t = 1:length(X)
                 XVel(i) = 2;
             
             elseif (XPrevState(i) > 20)
-                XVel = -2;
+                XVel(i) = -2;
             
             elseif (XPrevState(i) >= 0 && XPrevState(i) <= 20 ) 
-                XVel = XPrevVel - abs(normrnd(0,0.0625));
+                XVel(i) = XPrevVel(i) - abs(randn*0.0625^2);
             
             elseif (XPrevState(i) >= -20 && XPrevState(i) < 0)
-                XVel = XPrevVel + abs(normrnd(0,0.0625));
-            
+                XVel(i) = XPrevVel(i) + abs(randn*0.0625^2);
             end
+            
+            XPrevState(i) = XState(i);
+            XPrevVel(i) = XVel(i);
             
             %Update weight
             
-            ytP = 1/ (sqrt(2*pi) * 4)  * exp( -( XState(i) - xm1  )^2 / 2 * sigmaM^2 ) ...
-                  + 1/ (sqrt(2*pi) * 4)  * exp( -( XState(i) - xm2  )^2 / 2 * sigmaM^2 ) ... 
-                  + normrnd(0,0.003906) ;
+            ytP(i) = (1 / (sqrt(2*pi) * 4))  * exp( -( XState(i) - xm1  )^2 / (2 * sigmaM^2) ) ...
+                  + (1 / (sqrt(2*pi) * 4))  * exp( -( XState(i) - xm2  )^2 / (2 * sigmaM^2) ) ... 
+                  + randn*0.003906^2;
             
-            prob = 1 / (sqrt(2*pi) * normrnd(0,0.003906)) * exp ( - (ytP - sensorRead(i) )^2 / 2 * normrnd(0,0.003906) );
+            prob = (1 / (sqrt(2*pi) * 0.003906)) * exp ( - (ytP(i) - sensorRead(i) )^2 / (2 * (0.003906)^2 ));
             
             wtsUN(i) = wtsPrev(i) * prob;
+            wtsPrev(i) = wtsUN(i);
             
     end
     
-            %Normalize weights
-            wtsSum = 0;
+    %Normalize weights
+    wtsSum = 0;
+    Exp = 0;
+    CV = 0;
+    Index = zeros(1,M);
             
-            for k = 1:M
-                wtsSum = wtsSum + wtsUN(k); 
-            end
+    for k = 1:M
+        wtsSum = wtsSum + wtsUN(k) ;
+    end
             
-            for k = 1:M
-                wts(k) = wtsUN(k) / wtsSum ;  
-            end
+    for k = 1:M
+        wts(k) = wtsUN(k) / wtsSum ;  
+    end
             
+    for k = 1:M
+        %Expected Filter Output
+        Exp = Exp +  wts(k) *  XState(k);
+        
+        %Coefficient of variation
+        CV = CV + ((M * wts(k) - 1) ^ 2);
+    end
+    
+    output(t) = Exp;
+    CV = 1/M * CV;
             
+    %Effective Sampling Size
+    ESS = M / (1 + CV);
+    
+    %Resampling
+        if( ESS < 0.5 * M )
+                
+                %Cumulative Weights Q
+                Q(1) = wts(1); 
+                for k = 2:M
+                    Q(k) = Q(k-1) + wts(k); 
+                end
+                
+                %Guesses
+                for K = 1:M
+                    T(k) = abs(rand(1));
+                end
+                T(k+1) = 1;
+                
+                %Sorting the Guesses
+                T = sort(T);
+                
+                i = 1;
+                j = 1;
+                
+                while( i <= M )
+                
+                    if( T(i) < Q(j) )
+                        Index(i) = j;
+                        i = i+1;
+                    else
+                        j = j+1;
+                    end
+                    
+                end
+                
+                for i = 1:M
+                    XState(i) = XState(Index(i));
+                    XPrevState(i) = XState(i);
+                    wts(i) = 1/M;
+                    wtsPrev(i) = 1/M;
+                end
+                
+        end
 end
+
+X = 0 : length(actualPos) - 1;
+ 
+figure(1)
+% plot(X,actualPos);
+hold on
+plot(X,output)
+hold off
+xlabel("Samples");
+ylabel("Actual Position");
+legend("Actual Position", "Filter Output");
+% 
+% figure(2)
+% plot (X, sensorRead);
+% xlabel("Samples");
+% ylabel("Sensor Reading");
+% 
+% figure(3)
 
 disp("SUCCESS!!!!!!!")
