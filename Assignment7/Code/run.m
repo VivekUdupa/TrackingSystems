df31b221df50c1835b4194052c41c108ebfa3f84
VivekUdupa <vkoodli@clemson.edu>
2018-11-29 03:10:50 -0500

    Draft1

--- Assignment7/Code/run.m
+++ Assignment7/Code/run.m
@@ -1,12 +1,8 @@
 function [Prob, Best] = run()%Main function to execute the Viterbi algorithm
-    
 clc
 clear
-
     %% Initialization
-    %Order A        C       G       T
-%     H = [-2.322, -1.737, -1.737, -2.322];
-%     L = [-1.737, -2.322, -2.322, -1.737];
+    %Order     A    C    G     T
     H = log2( [0.2, 0.3, 0.3, 0.2] );
     L = log2( [0.3, 0.2, 0.2, 0.3] );
 
@@ -17,16 +13,14 @@ clear
     
     %Transition probabilities
     phh = -1;
-%     pll = -0.737;
     pll = log2(0.6);
     phl = -1;
-%     plh = -1.322;
     plh = log2(0.4);
     
     %Input sequence
     
-    IS = ['G', 'G', 'C', 'A', 'C', 'T', 'G', 'A', 'A' ];
-%     IS = ['T', 'C', 'A', 'G', 'C', 'G', 'G', 'C', 'T' ];
+%     IS = ['G', 'G', 'C', 'A', 'C', 'T', 'G', 'A', 'A' ]; %Sequence 1
+    IS = ['T', 'C', 'A', 'G', 'C', 'G', 'G', 'C', 'T' ]; %Sequence 2
     
     size = length(IS);
     
